af9016418cff195b2a3b796b30d7eb5090283f8d
Mark Marianelli <mark.marianelli@gmail.com>
2020-07-12 23:27:50 -0500

    adding .travis.yml and deploy.sh files

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '3.81.43.24',
+      host: '3.94.133.63',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
